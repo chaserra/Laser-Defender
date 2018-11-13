70734a2ea7fbf0abbb011e7b68f19c7710fcffd6
Charles Gerard Serrato <serrato.charles@gmail.com>
2018-11-13 21:08:50 +0800

    -Boss added (prefab, wave, path). -Modified enemy spawner script. +Added stops at level script.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour {
     int startingWave = 0;
     [SerializeField] float spawnDelay = 1.5f;
     [SerializeField] int activatesAtLevel = 1;
+    [SerializeField] int stopsAtLevel = 5;
     [SerializeField] bool looping = false;
 
     //Cached References
@@ -26,9 +27,14 @@ public class EnemySpawner : MonoBehaviour {
             yield return new WaitForSeconds(2f);
             Debug.Log(gameObject.name + " Activated!");
             for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++) {
-                var currentWave = waveConfigs[waveIndex];
-                yield return new WaitForSeconds(spawnDelay);
-                yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+                if(player.GetLaserLevel() >= stopsAtLevel) {
+                    Debug.Log(gameObject.name + " Deactivated!");
+                    yield break;
+                } else {
+                    var currentWave = waveConfigs[waveIndex];
+                    yield return new WaitForSeconds(spawnDelay);
+                    yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+                }
             }
         }
     }
@@ -45,6 +51,5 @@ public class EnemySpawner : MonoBehaviour {
         }
     }
 
-    //TODO rework waves. balance more
-    //TODO Final boss at level 5.
+    //TODO Make Boss Pathing (not self-destructing on last point).
 }
